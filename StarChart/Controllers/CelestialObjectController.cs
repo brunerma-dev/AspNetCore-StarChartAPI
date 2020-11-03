de5c55705884a51fb3e68ff2efe83ff5f736b224
Mike Bruner <brunerma@icloud.com>
2020-11-03 12:45:30 -0800

    Corrected module

---
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StarChart.Data;
+
+namespace StarChart.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class CelestialObjectController : Controller
+    {
+        public CelestialObjectController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        private readonly ApplicationDbContext _context;
+    }
+}
